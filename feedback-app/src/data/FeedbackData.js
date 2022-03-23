33c0f7966ed02913e53d1e6da9d04a9280a83206
Simodalstix <lastpolar@gmail.com>
2022-03-23 13:16:37 +1100

    Added FeedbackStats and Card components

---
+++ feedback-app/src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 8,
+    text: "this is just place holder text, while we test out the functionality of the app",
+  },
+  {
+    id: 2,
+    rating: 2,
+    text: "this is just place holder text, while we test out the functionality of the app",
+  },
+  {
+    id: 3,
+    rating: 5,
+    text: "this is just place holder text, while we test out the functionality of the app",
+  },
+];
+
+export default FeedbackData
