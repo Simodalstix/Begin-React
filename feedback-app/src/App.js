da117ee7fa8cf31344a93c2b7ff9e9b063f4dc7f
Simodalstix <lastpolar@gmail.com>
2022-03-29 15:46:42 +1100

    FrontEnd mostly done

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,35 +1,36 @@
-import { v4 as uuidv4 } from 'uuid'
-import { useState } from "react"
-import Header from "./components/Header"
-import FeedbackList from "./components/FeedbackList"
-import FeedbackData from "./data/FeedbackData"
-import FeedbackStats from "./components/FeedbackStats"
-import FeedbackForm from "./components/FeedbackForm"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
+import AboutPage from './Pages/AboutPage'
+import AboutIconLink from './components/AboutIconLink'
+import { FeedbackProvider } from './context/FeedbackContext'
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData)
-
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback])
-    console.log(newFeedback)
-  }
-
-  const deleteFeedback = (id) => {
-    if (window.confirm("Are you sure?")) {
-      setFeedback(feedback.filter((item) => item.id !== id))
-    }
-  }
-
   return (
-    <>
-      <Header />
-      <div className='container'>
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-      </div>
-    </>
+    <Router>
+      <FeedbackProvider>
+        <Header />
+        <div className='container'>
+          <Routes>
+            <Route
+              eact
+              path='/'
+              element={
+                <>
+                  <FeedbackForm />
+                  <FeedbackStats />
+                  <FeedbackList />
+                </>
+              }
+            ></Route>
+            <Route path='/about' element={<AboutPage />} />
+          </Routes>
+          <AboutIconLink />
+        </div>
+      </FeedbackProvider>
+    </Router>
   )
 }
 export default App
--- feedback-app/src/components/FeedbackForm.jsx
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -1,20 +1,31 @@
-import { useState } from "react"
-import RatingSelect from "./RatingSelect"
-import Card from "./shared/Card"
-import Button from "./shared/Button"
+import { useState, useContext, useEffect } from 'react'
+import RatingSelect from './RatingSelect'
+import Card from './shared/Card'
+import Button from './shared/Button'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackForm({ handleAdd }) {
-  const [text, setText] = useState("")
-  const [rating, setRating] = useState("10")
+function FeedbackForm() {
+  const [text, setText] = useState('')
+  const [rating, setRating] = useState('10')
   const [btnDisabled, setBtnDisabled] = useState(true)
-  const [message, setMessage] = useState("")
+  const [message, setMessage] = useState('')
+
+  const { addFeedback, feedbackEdit, updateFeedback } = useContext(FeedbackContext)
+
+  useEffect(() =>{
+    if(feedbackEdit.edit === true) {
+      setBtnDisabled(false)
+      setText(feedbackEdit.item.text)
+      setRating(feedbackEdit.item.rating)
+    }
+  }, [feedbackEdit])
 
   const handleTextChange = (e) => {
-    if (text === "") {
+    if (text === '') {
       setBtnDisabled(true)
       setMessage(null)
     } else if (text.trim().length < 10) {
-      setMessage("Text must be at least 10 characters")
+      setMessage('Text must be at least 10 characters')
       setBtnDisabled(true)
     } else {
       setMessage(null)
@@ -31,7 +42,13 @@ function FeedbackForm({ handleAdd }) {
         text,
         rating,
       }
-      handleAdd(newFeedback)
+
+      if (feedbackEdit.edit === true){
+        updateFeedback(feedbackEdit.item.id, newFeedback)
+      } else {
+        addFeedback(newFeedback)
+      }
+      
       setText('')
     }
   }
